51e81df2d0639765f465dfbbfcdc7f057bd990d0
Zulwiyoza Putra <zulwiyozaputra@zulwiyozas-mbp.lan>
2016-11-22 03:27:46 -0800

    Working

--- Roshambo/ResultViewController.swift
+++ Roshambo/ResultViewController.swift
@@ -9,11 +9,42 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    var opponentStatus: Int?
+    var playerStatus: Int?
+    var resultStatus: Int?
 
+    @IBOutlet weak var resultStatusLabel: UILabel!
+    
+    @IBOutlet weak var detailInformationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        resultSetup()
+        detailInformation()
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func resultSetup() -> Void {
+        if resultStatus == 1 {
+            resultStatusLabel.text = "You win"
+        } else if resultStatus == 2 {
+            resultStatusLabel.text = "Draw"
+        } else if resultStatus == 3 {
+            resultStatusLabel.text = "You Lose"
+        }
+    }
+    
+    func detailInformation() -> Void {
+        if opponentStatus == 1 {
+            detailInformationLabel.text = "Opponent throw a paper"
+        } else if opponentStatus == 2 {
+            detailInformationLabel.text = "Opponent throw a rock"
+        } else if opponentStatus == 3 {
+            detailInformationLabel.text = "Opponent throw a scissor"
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +52,10 @@ class ResultViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func tryAgain(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
--- Roshambo/ViewController.swift
+++ Roshambo/ViewController.swift
@@ -7,9 +7,80 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
-
+    
+    var opponentValue: Int?
+    var playerValue: Int?
+    var winningStatus: WinningStatus?
+    
+    func randomNumber() -> Int {
+        let startingNumber = 1
+        let randomNumber = GKRandomSource.sharedRandom().nextInt(upperBound: 3) + startingNumber
+        return randomNumber
+    }
+    
+    func setupOpponentValue() -> Void {
+        opponentValue = randomNumber()
+        print("Opponent value is \(opponentValue!)")
+    }
+    
+    func setupPlayerValue(sender: UIButton) -> Void {
+        switch ButtonType(rawValue: sender.tag)! {
+        case .paper:
+            playerValue = 1
+        case .rock:
+            playerValue = 2
+        case .scissor:
+            playerValue = 3
+        }
+        
+        print("Player value is \(playerValue!)")
+    }
+    
+    func setupComparison() -> Void {
+        if playerValue == 1 && opponentValue == 2 {
+            winningStatus = .win
+        } else if playerValue == 1 && opponentValue == 3 {
+            winningStatus = .lose
+        } else if playerValue == 2 && opponentValue == 3 {
+            winningStatus = .win
+        } else if playerValue == 2 && opponentValue == 1 {
+            winningStatus = .lose
+        } else if playerValue == 3 && opponentValue == 1 {
+            winningStatus = .win
+        } else if playerValue == 3 && opponentValue == 2 {
+            winningStatus = .lose
+        } else if playerValue == opponentValue {
+            winningStatus = .draw
+        }
+        
+        print((winningStatus?.rawValue)!)
+    }
+    
+    enum ButtonType: Int {
+        case paper = 1
+        case rock = 2
+        case scissor = 3
+    }
+    
+    enum WinningStatus: Int {
+        case win = 1
+        case draw = 2
+        case lose = 3
+    }
+    
+    @IBAction func playerValueInput(_ sender: UIButton) {
+        print("Player Value Input Button Tapped")
+        
+        setupOpponentValue()
+        setupPlayerValue(sender: sender)
+        setupComparison()
+        
+        performSegue(withIdentifier: "roshambo", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +89,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.opponentStatus = self.opponentValue
+        resultViewController.playerStatus = self.playerValue
+        resultViewController.resultStatus = self.winningStatus?.rawValue
+        
+        
     }
 
 
